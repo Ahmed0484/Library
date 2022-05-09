7d37ac54756efe80286d57ddfc78f692a97cacd6
Ahmed0484 <a_elshazly_08@hotmail.com>
2022-05-09 23:26:38 +0200

    Add project files.

---
+++ Library/Models/SeedData.cs
@@ -0,0 +1,25 @@
+﻿using Library.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider sp)
+        {
+            using (var ctx =
+                new LibraryContext(sp.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if (ctx.Book.Any()) return;
+
+                ctx.AddRange(
+                    new Book {Title="Blazor",Author="Ali Ahmed" },
+                    new Book { Title = "Razor", Author = "Ahmed Ameen" },
+                    new Book { Title = "Browser", Author = "Karem Mohram" },
+                    new Book { Title = "C#", Author = "Ali Ahmed" }
+                    );
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
